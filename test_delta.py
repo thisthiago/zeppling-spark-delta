3876110479b8ff6c9b87d0ee4bef5a6f89f59b4e
thisthiago <60991003+thisthiago@users.noreply.github.com>
2025-03-25 18:23:17 -0300

    adicionando teste leitura do s3

--- test_delta.py
+++ test_delta.py
@@ -14,3 +14,23 @@ df.createOrReplaceTempView('tb_teste')
 df.write.format("delta").mode("overwrite").save("/tmp/delta_table")
 
 print("Tabela Delta criada com sucesso!")
+
+#Teste com leitura do s3
+#%pyspark
+accessKeyId=""
+secretAccessKey=""
+
+import os
+from pyspark.sql import SparkSession
+
+# Define as variáveis de ambiente temporariamente
+os.environ['AWS_ACCESS_KEY_ID'] = accessKeyId
+os.environ['AWS_SECRET_ACCESS_KEY'] = secretAccessKey
+
+spark = SparkSession.builder \
+    .appName("DeltaS3Read") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .getOrCreate()
+spark.read.format("delta").load("s3a://...")
